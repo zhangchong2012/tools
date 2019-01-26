bf503a978c52b40d54c11ac4c86ceba6c61d77c8
张充 <zchong@meizu.com>
2019-01-26 16:08:25 +0800

    [tooks] update v1.0.0

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
-# 工具使用书
+# xadb工具说明书
 
-## adbutils
+## 目标
 
-**注意事项**
- 第一次使用本脚本命令必须用usb连接手机
+WiFi环境下,放弃usb线条,远程调试
 
-命令说明:
-1.连接设备:
-	 ./adbutils.sh -p ip
-2.关闭连接:
-	 ./adbutils.sh -c
-3.查看[usb/wifi]连接设备ip:
-	 ./adbutils.sh -l
+## 使用步骤
+
+1. 下载 adbutils.sh, 赋予执行权限 chmod +x adbutils.sh
+1. 安装xadb
+	1.  adbutils -i
+1. 找到手机ip,连接设备 xadb -p ip
+1. 其他
+	1. xadb -h 帮助
+	1. xadb -c 断开所有远程设备
+
+## 错误处理
+
+### /usr/local/bin/xadb: line 18: adb: command not found
+解决方案:
+  1. 安装Homebrew
+    1. https://brew.sh/
+  2. ​brew cask install android-platform-tools​
+  3. ​adb devices​ 有正确输出即可.
+
+### xadb -p xx.xx.xx.xx:出现confuse xxx, 
+多试几次命令,手机的调试服务还在启动
+第一次使用本脚本命令必须用usb连接手机,以启动远程服务
--- adbutils.sh
+++ adbutils.sh
@@ -3,22 +3,38 @@ startadb(){
 	adb tcpip 5555 2> /dev/null
 	#adb shell setprop service.adb.tcp.port 5555 2> /dev/null
 	adb connect ${ip}
-	adb devices
 }
 
 closeadb(){
- for line in `adb devices`
+ for line in `adb devices | awk '{print $1}' | sed -n '2,$p'`
 	do
-			adb disconnect $line 2> /dev/null
+		if [ -n "$line" ]; then 
+			adb disconnect $line
+		fi
 	done
-	adb devices
 }
 
 findadbs(){
-	adb shell ip -f inet addr show wlan0
+	echo "List of devices ips"
+	for line in `adb devices | awk '{print $1}' | sed -n '2,$p'`
+	do
+		if [ -n "$line" ]; then 
+			thisIp=`adb -s $line shell ip -f inet addr show wlan0 | grep inet | awk '{print $2}'`
+			echo -e "$line\t${thisIp}"
+		fi
+	done
 }
 
-while getopts "p:c h l" arg #选项后面的冒号表示该选项需要参数
+install(){
+	SHELL_FILE=$(cd "$(dirname "$0")";pwd)/${0}
+	cp -r ${SHELL_FILE} /usr/local/bin/xadb
+}
+
+listDevices(){
+	adb devices
+}
+
+while getopts "p:c h l i d" arg
 do
         case $arg in
              p)
@@ -31,14 +47,24 @@ do
 						 l)
 						 		findadbs
 						 		;;
+						 i)
+								install
+						 		;;
+						 d)
+								listDevices
+						 		;;
              h)
-						 		echo -e "**注意事项**\n 第一次使用本脚本命令必须用usb连接手机\n"
-								echo -e "命令说明:"
-                echo -e "1.连接设备:\n\t ${0} -p ip"
-								echo -e "2.关闭连接:\n\t ${0} -c"
-								echo -e "3.查看[usb/wifi]连接设备ip:\n\t ${0} -l"
+								echo -e "\n命令说明:"
+								echo -e "1.安装xadb:\n\t ${0} -i\n"
+                echo -e "2.连接设备:\n\t ${0} -p ip\n"
+								echo -e "3.关闭连接:\n\t ${0} -c\n"
+								echo -e "4.查看[usb/wifi]连接设备ip:\n\t ${0} -l\n"
+								echo -e "5.查看[usb/wifi]连接设备:\n\t ${0} -d\n"
+								echo -e "6.帮助:\n\t ${0} -h\n"
+						 		echo -e "**注意事项**\n\t 第一次使用本脚本命令必须用usb连接手机\n"
+								echo -e "如有其他帮助,请联系作者:\n\t zhangchong.ff@bytedance.com"
                 ;;
-             ?)  #当有不认识的选项的时候arg为?
+             ?) 
             echo "unkonw argument"
         exit 1
         ;;
